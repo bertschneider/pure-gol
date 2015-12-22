3a8bcdcbf0651471c3337b1593772f3167de8950
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2015-12-22 01:59:30 +0100

    BLING BLING

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,19 @@
 module Main where
 
-import Prelude
-import Control.Monad.Eff
-import Control.MonadPlus
+import           Control.Monad.Eff
+import           Control.Monad.Eff.Console
+import           Control.Monad.ST
+import           Control.MonadPlus
+import           DOM.Timer
+import           Data.Int (toNumber)
+import           Data.List
+import           Data.Maybe
 import qualified Data.Set as S
-import Data.List
+import           Data.Traversable
+import           Prelude
+
+import           Graphics.Canvas (getContext2D, getCanvasElementById, Canvas())
+import           Graphics.Canvas.Free
 
 data IsAlive = Alive | Dead
 
@@ -16,7 +25,7 @@ instance showWorld :: Show World where
 newtype Cell = Cell {x :: Int, y :: Int}
 
 instance showCell :: Show Cell where
-  show (Cell c) = "[" <> show c.x <> "|" <> show c.y <> "]" 
+  show (Cell c) = "[" <> show c.x <> "|" <> show c.y <> "]"
 
 instance ordCell :: Ord Cell where
   compare (Cell c1) (Cell c2)
@@ -31,7 +40,7 @@ neighbours (Cell c) = do
   x <- -1..1
   y <- -1..1
   guard (x /= 0 || y /= 0)
-  return $ Cell {x:(c.x + x), y:(c.y + y)}
+  return $ Cell {x: (c.x + x), y: (c.y + y)}
 
 candidates :: World -> List Cell
 candidates (World w) =
@@ -45,7 +54,7 @@ isAlive :: World -> Cell -> IsAlive
 isAlive (World w) c =
   if S.member c w
   then Alive
-  else Dead                              
+  else Dead
 
 evolve :: World -> World
 evolve w =
@@ -53,15 +62,41 @@ evolve w =
   sort >>>
   group >>>
   filter shouldLive' >>>
-  toWorld $ w 
+  toWorld $ w
  where
     isAlive' = isAlive w
     shouldLive' l@(Cons h _) = shouldLive (isAlive' h) (length l)
     toCell (Cons h _) = h
     toWorld = map toCell >>> S.fromList >>> World
-        
-w3 = (World (S.fromList (Cons (Cell {x: 1, y:1})
+
+cells1 = (Cons (Cell {x: 1, y:1})
                          (Cons (Cell {x:1, y:2})
                           (Cons (Cell {x:1, y:3})
-                           Nil)))))
+                           Nil)))
 
+w3 = (World (S.fromList cells1))
+
+render :: forall eff. World -> Eff (canvas :: Canvas | eff) Unit
+render (World w) = do
+  Just x <- getCanvasElementById "canvas"
+  ctx <- getContext2D x
+  runGraphics ctx do
+    clearWorld
+    traverse livingCell cells
+  return unit
+  where
+    cells = S.toList w
+
+clearWorld :: Graphics Unit
+clearWorld = clearRect {x: -300.0, y: -300.0, w: 600.0, h: 600.0}
+
+livingCell :: Cell -> Graphics Unit
+livingCell (Cell c)  = do
+  setFillStyle "#FF0000"
+  fillRect {x: (toNumber c.x) * 10.0, y: (toNumber c.y) * 10.0, w: 10.0, h: 10.0}
+
+initialWorld = w3
+
+main = do
+  state <- newSTRef initialWorld
+  interval 1000 (modifySTRef state evolve >>= render)
